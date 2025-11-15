af84a700077bfa59e69cddea784fcec331315d6b
daenekint <daenekint.yvo@gmail.com>
2025-11-15 23:57:45 +0100

    script.js

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ facebook:
 https://www.facebook.com/sharer.php?u=[post-url]
 
 */
+
+const facebookbtn = documet.queryselector(".facebook-btn");
+const whatsappbtn = document.queryselector(".whatsapp-btn");
+
+function init() {
+
+}
+
+init();
